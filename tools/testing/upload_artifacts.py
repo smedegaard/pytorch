92eb856ed51cf44768ee4a194e49603aed5b91a7
Catherine Lee <csl@fb.com>
2024-10-14 14:57:39 -0700

    tc

--- tools/testing/upload_artifacts.py
+++ tools/testing/upload_artifacts.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 import glob
 import os
 import time
+from typing import List
 import zipfile
 from pathlib import Path
 
@@ -18,49 +19,57 @@ def get_s3_resource():
     return boto3.client("s3")
 
 
-def zip_artifacts() -> str:
-    """Zip up the artifacts."""
-    file_suffix = os.environ.get("ARTIFACTS_FILE_SUFFIX")
-    if not file_suffix:
-        raise ValueError("ARTIFACTS_FILE_SUFFIX is not set")
-    file_name = f"{REPO_ROOT}/test-reports-{file_suffix}.zip"
+def zip_artifact(file_name: str, paths: List[str]) -> str:
+    """Zip the files in the paths listed into file_name. The paths will be used
+    in a glob and should be relative to REPO_ROOT."""
 
     with zipfile.ZipFile(file_name, "w") as f:
-        for file in glob.glob(f"{REPO_ROOT}/test/**/*.xml", recursive=True):
-            f.write(file, os.path.relpath(file, REPO_ROOT))
-        for file in glob.glob(f"{REPO_ROOT}/test/**/*.csv", recursive=True):
-            f.write(file, os.path.relpath(file, REPO_ROOT))
-
-    return file_name
+        for path in paths:
+            for file in glob.glob(f"{REPO_ROOT}/{path}", recursive=True):
+                f.write(file, os.path.relpath(file, REPO_ROOT))
 
 
-def upload_to_s3_artifacts(file_name: str) -> None:
+def upload_to_s3_artifacts() -> None:
     """Upload the file to S3."""
     workflow_id = os.environ.get("GITHUB_RUN_ID")
-    if not workflow_id:
-        raise ValueError("GITHUB_RUN_ID is not set")
+    workflow_run_attempt = os.environ.get("GITHUB_RUN_ATTEMPT")
+    file_suffix = os.environ.get("ARTIFACTS_FILE_SUFFIX")
+    if not workflow_id or not workflow_run_attempt or not file_suffix:
+        raise ValueError(
+            "GITHUB_RUN_ID, GITHUB_RUN_ATTEMPT, or ARTIFACTS_FILE_SUFFIX not set"
+        )
 
+    test_reports_zip_path = f"{REPO_ROOT}/test-reports-{file_suffix}.zip"
+    zip_artifact(test_reports_zip_path, ["test/**/*.xml", "test/**/*.csv"])
+    test_logs_zip_path = f"{REPO_ROOT}/logs-{file_suffix}.zip"
+    zip_artifact(test_logs_zip_path, ["test/**/*.log"])
+
+    s3_prefix = f"pytorch/pytorch/{workflow_id}/{workflow_run_attempt}/artifact"
     get_s3_resource().upload_file(
-        file_name,
+        test_reports_zip_path,
         "gha-artifacts",
-        f"pytorch/pytorch/{workflow_id}/{Path(file_name).name}",
+        f"{s3_prefix}/{Path(test_reports_zip_path).name}",
+    )
+    get_s3_resource().upload_file(
+        test_logs_zip_path,
+        "gha-artifacts",
+        f"{s3_prefix}/{Path(test_logs_zip_path).name}",
     )
     get_s3_resource().put_object(
         Body=b"",
         Bucket="gha-artifacts",
-        Key=f"catttest_deleteme/{workflow_id}.txt",
+        Key=f"workflows_failing_pending_upload/{workflow_id}.txt",
     )
 
 
 def zip_and_upload_artifacts(failed: bool) -> None:
     # not thread safe but correctness doesn't really matter for this,
     # approximate is good enough
-    # Upload if a test failed or every 10 minutes
+    # Upload if a test failed or every 20 minutes
     global LAST_UPDATED
 
-    if failed or time.time() - LAST_UPDATED > 10 * 60:
-        file_name = zip_artifacts()
-        upload_to_s3_artifacts(file_name)
+    if failed or time.time() - LAST_UPDATED > 20 * 60:
+        upload_to_s3_artifacts()
         LAST_UPDATED = time.time()
 
 
